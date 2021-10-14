f23e03777b0cf4fe1f4857954ad125841010519b
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-10-14 14:41:03 -0400

    Unit 2: Layouts
    Display a scrollable list.
    
    Use RecyclerView to display a scrollable list

---
+++ Kotlin/Android Basic in Kotlin/Unit_02-Layouts/Display a scrollable list/RecyclerView/app/src/main/java/br/com/pedroabreudev/affirmations/data/Datasource.kt
@@ -0,0 +1,21 @@
+package br.com.pedroabreudev.affirmations.data
+
+import br.com.pedroabreudev.affirmations.R
+import br.com.pedroabreudev.affirmations.model.Affirmation
+
+class Datasource {
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+        )
+    }
+}
---
+++ Kotlin/Android Basic in Kotlin/Unit_02-Layouts/Display a scrollable list/RecyclerView/app/src/main/java/br/com/pedroabreudev/affirmations/model/Affirmation.kt
@@ -0,0 +1,3 @@
+package br.com.pedroabreudev.affirmations.model
+
+data class Affirmation(val stringResourceId: Int)
